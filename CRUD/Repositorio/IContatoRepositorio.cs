9d3a7d656d85ca18d1dbdf7bd3aae5ba141c85bb
Victor Brum <83709796+brumvictor@users.noreply.github.com>
2023-08-02 12:11:33 -0300

    Creating and listing contacts

---
+++ CRUD/Repositorio/IContatoRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.Repositorio
+{
+    public interface IContatoRepositorio
+    {
+        List<ContatoModel> BuscarTodos();
+
+        ContatoModel Adicionar(ContatoModel contato);
+    }
+}
